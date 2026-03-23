15afa82bc84dfec54200aacb25cf2a85a723680b
Muchiri <llwmuchiri@gmail.com>
2026-03-23 19:28:02 +0300

    CORS issue fix - Auth 2

--- Beginner/To-do list/Server/router.ts
+++ Beginner/To-do list/Server/router.ts
@@ -5,6 +5,27 @@ export default function Router(
   request: IncomingMessage,
   response: ServerResponse<IncomingMessage>,
 ) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS",
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With",
+  );
+  response.setHeader("Access-Control-Max-Age", "86400");
+
+  if (request.method == "OPTIONS") {
+    response.writeHead(204, {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-Requested-With",
+    });
+    return response.end();
+  }
+
   const requestUrl = new URL(request.url!, `http://${request.headers.host}`),
     pathName = requestUrl.pathname.split("/").filter(Boolean);
 
